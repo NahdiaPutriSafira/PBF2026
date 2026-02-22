be85f144414eb22a420afd8cc684d29930dc522d
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-22 22:59:38 +0700

    P2_Jobsheet Routing, Nested Routing, Dynamic Routing, dan Layouting pada Next.js (Pages Router)_Add AppShell component with Navbar integration

---
+++ praktikum/my-app/src/component/layouts/Appshell/index.tsx
@@ -0,0 +1,16 @@
+import Navbar from "../navbar";
+
+type AppShellProps = {
+    children: React.ReactNode;
+}
+
+const AppShell = (props:AppShellProps) => {
+    const {children} = props;
+    return (
+        <main>
+            <Navbar />
+            {children}
+        </main>
+        
+    );
+};
