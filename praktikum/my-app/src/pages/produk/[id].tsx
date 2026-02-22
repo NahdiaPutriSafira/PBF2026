ee35cd8d9e73bda1ae370e84af2d8305e623450b
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-22 21:01:33 +0700

    P2_JSRouting, Nested Routing, Dynamic Routing, dan Layouting pada Next.js (Pages Router)_Modifikasi [id].tsx untuk mengambil parameter id dari URL

--- praktikum/my-app/src/pages/produk/[id].tsx
+++ praktikum/my-app/src/pages/produk/[id].tsx
@@ -1,15 +1,15 @@
 import { useRouter } from "next/router";
 
 const DetailProdukPage = () => {
-    const router = useRouter();
-    console.log(router);
+    // const router = useRouter();
+    // console.log(router);
 
-    const { id } = router.query;
+    const { query } = useRouter();
 
     return (
         <div>
             <h1>Detail Produk</h1>
-            <p>Produk yang dipilih: {id}</p>
+            <p>Produk yang dipilih: {query.id}</p>
         </div>
     );
 };
