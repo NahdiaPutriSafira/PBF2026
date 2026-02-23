02a0ba2e412a8591f95b7ee7926bcc5e36a7a530
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-23 10:29:59 +0700

    P2_JS Link Navigation_Modifikasi [...slug].tsx untuk menampilkan nilai query

--- praktikum/my-app/src/pages/shop/[...slug].tsx
+++ praktikum/my-app/src/pages/shop/[...slug].tsx
@@ -2,12 +2,15 @@ import {useRouter} from "next/router";
 
 
 const halamanToko = () => {
-    const router = useRouter();
-    console.log(router);
-    
+    // const router = useRouter();
+    // console.log(router);
+    const {query} = useRouter();
     return (
         <div>
             <h1>Halaman Toko</h1>
+            <p>
+        Toko: {`${query.slug && query.slug[0]} - ${query.slug && query.slug[1]}`}
+      </p> {/* menggunakan backtick bukan petik satu */}
         </div>
     );
 };
