641a87e798d4eff073d8c102e8735e7fc4a0484d
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-23 10:55:30 +0700

    P2_JS Link Navigation_Validasi Parameter

--- praktikum/my-app/src/pages/shop/[[...slug]].tsx
+++ praktikum/my-app/src/pages/shop/[[...slug]].tsx
@@ -5,12 +5,15 @@ const halamanToko = () => {
     // const router = useRouter();
     // console.log(router);
     const {query} = useRouter();
+    const slug = query.slug;
+
     return (
         <div>
             <h1>Halaman Toko</h1>
+
             <p>
-            Toko: {`${query.slug && query.slug[0]} - ${query.slug && query.slug[1]}`}
-            </p> {/* menggunakan backtick bukan petik satu */}
+            Kategori: {slug ? slug[0] : "Semua Kategori"}
+            </p> 
       
 
         </div>
