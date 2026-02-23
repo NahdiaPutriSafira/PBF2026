e21e319dd4f08af7b472c8a7b09c9f7d4db32cef
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-23 11:22:36 +0700

    P2_JS Link Navigation_Navigasi Imperatif (router.push)

--- praktikum/my-app/src/pages/produk/index.tsx
+++ praktikum/my-app/src/pages/produk/index.tsx
@@ -1,9 +1,18 @@
-const ProdukPage = () => {
-    return (
-        <div>
-            Halaman Produk
-        </div>
-    );
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const produk = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  // useEffect(() => {
+  //   if (!isLogin) {
+  //     push("/auth/login");
+  //   }
+  // }, []);
+
+  return (
+    <div>Produk User Page</div>
+  );
 };
 
-export default ProdukPage;
+export default produk;
