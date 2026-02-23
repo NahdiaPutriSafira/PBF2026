beae5134601138d30d3dc6b974388f167bc559c6
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-23 10:42:19 +0700

    P2_JS Link Navigation_Penyesuaian Catch-All Route

--- praktikum/my-app/src/pages/shop/[...slug].tsx
+++ praktikum/my-app/src/pages/shop/[...slug].tsx
@@ -8,13 +8,11 @@ const halamanToko = () => {
     return (
         <div>
             <h1>Halaman Toko</h1>
-            {/*<p>
-        Toko: {`${query.slug && query.slug[0]} - ${query.slug && query.slug[1]}`}
-      </p> {/* menggunakan backtick bukan petik satu */}
-      <p>
-        Toko: {Array.isArray (query.slug)? query.slug.join("-"): query.slug}
-      </p>
+            <p>
+            Toko: {`${query.slug && query.slug[0]} - ${query.slug && query.slug[1]}`}
+            </p> {/* menggunakan backtick bukan petik satu */}
       
+
         </div>
     );
 };
