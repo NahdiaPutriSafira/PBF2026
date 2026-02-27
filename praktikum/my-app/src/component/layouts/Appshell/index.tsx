43cf6083256c4f5e1b3cb238fff9aa1c155c161b
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-27 18:35:07 +0700

    P3_JS Layout_ Refactor AppShell component and add login page with styles

--- praktikum/my-app/src/component/layouts/Appshell/index.tsx
+++ praktikum/my-app/src/component/layouts/Appshell/index.tsx
@@ -1,27 +1,29 @@
+import { useRouter } from "next/router";
 import Navbar from "../navbar";
+
+
+const disableNavbar = ['/auth/login', '/auth/register'];
 
 type AppShellProps = {
   children: React.ReactNode;
 };
 
-const AppShell = ({ children }: AppShellProps) => {
-  return (
-    <>
-      <Navbar />
 
-      <main style={{ minHeight: "80vh", padding: "20px" }}>
-        {children}
-      </main>
 
-      <footer style={{ 
-        padding: "15px", 
-        textAlign: "center", 
-        backgroundColor: "#f1f1f1" 
-      }}>
-        © 2026 Nahdia Putri Safira | TI3D
-      </footer>
-    </>
-  );
+const AppShell = (props : AppShellProps) => {
+
+
+
+const {children} = props;
+const {pathname} = useRouter();
+return (
+  <main>
+    {!disableNavbar.includes(pathname) && <Navbar />}
+    {children}
+
+  </main>
+);
+  
 };
 
 export default AppShell;
