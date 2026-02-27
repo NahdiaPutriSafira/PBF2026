550d96267ea3ba2d8657edb5a3695008f4e8a416
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-27 16:40:54 +0700

    P3_JS Styling_ Modifikasi kode index.tsx pada folder navbar

--- praktikum/my-app/src/component/layouts/navbar/index.tsx
+++ praktikum/my-app/src/component/layouts/navbar/index.tsx
@@ -1,7 +1,12 @@
+import syles from './navbar.module.css';
+
+
 const Navbar = () => {
     return (
-        <div className="navbar">
-            <h1>navbar Component</h1>
+        <div className={syles.navbar}>
+            <div>
+                 <h1>navbar Component</h1>
+            </div>
         </div>
     );
 };
