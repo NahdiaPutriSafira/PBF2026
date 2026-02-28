d8701017736d1e267c6da0091cd4ad8d0a666243
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-28 10:22:16 +0700

    P3_JS Styling_ Refactor Navbar component structure and update styles

--- praktikum/my-app/src/component/layouts/navbar/index.tsx
+++ praktikum/my-app/src/component/layouts/navbar/index.tsx
@@ -2,13 +2,13 @@ import syles from './navbar.module.css';
 
 
 const Navbar = () => {
-    return (
+    return(
         <div className={syles.navbar}>
-            <div>
-                 <h1>navbar Component</h1>
+            <div className="big">
+                Navbar
             </div>
         </div>
-    );
+    )
 };
 
 export default Navbar;
