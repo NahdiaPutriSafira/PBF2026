5639bb27c5e66e53fa874f9b1c8f0a7513f4ba2a
Nahdia Putri <nptrsfr1808@gmail.com>
2026-02-28 10:50:44 +0700

    P3_JS Styling_ Refactor login component styles for improved readability

--- praktikum/my-app/src/pages/views/auth/login/index.tsx
+++ praktikum/my-app/src/pages/views/auth/login/index.tsx
@@ -12,22 +12,13 @@ const TampilanLogin = () => {
     }
 
     return (
-        <div className={styles.login}>
-            <h1>Halaman Login</h1>
-            <button onClick={() => handleLogin()}>Login</button> <br />
-            <h1
-                style={{
-                    color: "red",
-                    border: "1px solid red",
-                    borderRadius: "5px",
-                    padding: "5px"
-                }}
-            >
-                belum punya akun?
-            </h1>
-            <Link href={"/auth/register"}>Ke Halaman Register</Link>
-        </div>
-    );
+      <div className={styles.login}>
+        <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
+        <button onClick={() => handleLogin()}>Login</button> <br />
+        <h1 style={{color:"red",border:"1px solid red",borderRadius:"5px",padding:"5px"}}> belum punya akun</h1>
+        <Link href={"/auth/register"}>Ke Halaman Register</Link>
+      </div>
+  );
 };
 
 export default TampilanLogin;
