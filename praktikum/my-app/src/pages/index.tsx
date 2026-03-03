ac2b8be950544c7be908c6c93319206e6c902d95
Nahdia Putri <nptrsfr1808@gmail.com>
2026-03-03 10:54:07 +0700

    P3_JS Cusrom error&document_Langkah1 dan 2

---
+++ praktikum/my-app/src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <head>
+      <title>Praktikum Next.js pages Router</title>
+    </head>
+    
+      <h1>Praktikum Next.js pages Router</h1> <br /> 
+       <p>Mahasiswa D4 Pengembangan Web</p>
+      
+      {/* <h1>Halaman Utama</h1>
+      <p>Selamat datang di website saya</p>
+
+      <Link href="/about">
+        <button>Ke Halaman About</button>
+      </Link> */}
+      
+    </>
+  )
+}
