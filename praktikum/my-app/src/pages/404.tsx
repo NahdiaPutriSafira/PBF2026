16ed28f6d3033a30a5b62659af9fcecd9098cf34
Nahdia Putri <nptrsfr1808@gmail.com>
2026-03-03 11:02:41 +0700

    P3_JS Custom error&document_Langkah4

---
+++ praktikum/my-app/src/pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+    return (
+        <div>
+            <h1> 404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang anda cari tidak ada</p>
+        </div>
+    );
+};
+export default Custom404;
