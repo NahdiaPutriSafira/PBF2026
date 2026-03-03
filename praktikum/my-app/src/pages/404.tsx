424988f763f5f541d242df68fcc7e14a13e234b3
Nahdia Putri <nptrsfr1808@gmail.com>
2026-03-03 11:54:52 +0700

    P3_JS Custom error & document_Langkah6

--- praktikum/my-app/src/pages/404.tsx
+++ praktikum/my-app/src/pages/404.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/404.module.scss";
 const Custom404 = () => {
     return (
         <div className={styles.error}>
+            <img src="/page-not-found.svg" alt="404" className={styles.error_image}/>
             <h1> 404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf, halaman yang anda cari tidak ada</p>
         </div>
