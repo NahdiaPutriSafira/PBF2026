ca459e1f96fc0c786c53081d6378d88dd45d50e9
Nahdia Putri <nptrsfr1808@gmail.com>
2026-03-03 18:46:58 +0700

    feat: add Firebase SDK and initialize configuration

---
+++ praktikum/my-app/src/pages/utlis/db/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApeohwRV_sBL4ebM6OmP4-8b8x9oy4dAQ",
+  authDomain: "framework-next-cd20a.firebaseapp.com",
+  projectId: "framework-next-cd20a",
+  storageBucket: "framework-next-cd20a.firebasestorage.app",
+  messagingSenderId: "287878216164",
+  appId: "1:287878216164:web:dbe056e9346cf160d16ff1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
