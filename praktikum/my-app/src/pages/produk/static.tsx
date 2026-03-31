6717fc181f13bf143e40673bb18c70daecb60c01
Nahdia Putri <nptrsfr1808@gmail.com>
2026-03-31 11:34:00 +0700

    JS 12 Incremental Static Regeneration (ISR)_Implementasi ISR Otomatis

--- praktikum/my-app/src/pages/produk/static.tsx
+++ praktikum/my-app/src/pages/produk/static.tsx
@@ -13,8 +13,11 @@ const halamanProdukStatic = (props:{products:ProductType[]}) => {
 export default halamanProdukStatic;
 
 export async function getStaticProps() {
-     const products = await retrieveProducts("products");
-      console.log("DATA PRODUCTS:", products);
+    const res = await fetch('http://127.0.0.1:3000/api/produk')
+
+    const response: {data: ProductType[] } = await res.json();
+     //const products = await retrieveProducts("products");
+      //console.log("DATA PRODUCTS:", products);
     //const res = await fetch('http://127.0.0.1:3000/api/produk');
     // const response: ProductType[] = await res.json();
     //const response: {data: ProductType[] } = await res.json();
@@ -24,9 +27,10 @@ export async function getStaticProps() {
 
         props: {
             //products: response.data,
-            products: products,
+            products: response.data,
 
         },
+        revalidate: 10, //Revalidate data setiap 10 detik
     };
     
 }
