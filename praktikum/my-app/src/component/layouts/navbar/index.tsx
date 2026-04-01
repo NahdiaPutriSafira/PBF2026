41c4fd1728129c19be5bb9040077642e3d7bc07e
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-01 15:54:36 +0700

    JS14_Implement conditional rendering for Sign In/Sign Out buttons in Navbar

--- praktikum/my-app/src/component/layouts/navbar/index.tsx
+++ praktikum/my-app/src/component/layouts/navbar/index.tsx
@@ -3,14 +3,21 @@ import {signIn, signOut, useSession } from "next-auth/react"
 
 
 const Navbar = () => {
-    return(
-        <div className={syles.navbar}>
-            <div className="big">
-                Navbar
-            </div>
-            <button onClick= {() => signIn()}>Sign In</button>
-        </div>
-    );
+  const { data } = useSession()
+  //const { data: session } = useSession()
+ // console.log("session", session)
+  return (
+    <div className={syles.navbar}>
+      <div className="big">
+        Navbar
+      </div>
+      {data ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn()}>Sign In</button>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
