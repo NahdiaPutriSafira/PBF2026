103349a0ce36d32d9d95cd24f0ee42d6907209d2
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-05 21:14:19 +0700

    JS16_Bagian 4

--- praktikum/my-app/src/views/auth/login/index.tsx
+++ praktikum/my-app/src/views/auth/login/index.tsx
@@ -45,6 +45,7 @@ const TampilanLogin = () => {
   };
 
   return (
+    <>
     <div className={styles.login}>
       {/* Tampilkan pesan error di UI. */}
       {error && <p className={styles.login__error}>{error}</p>}
@@ -98,6 +99,7 @@ const TampilanLogin = () => {
         </p>
       </div>
     </div>
+    </>
   );
 };
 
