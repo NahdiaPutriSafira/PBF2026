4ee55678cefb9c2702687bb12be0795b57c634b9
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-06 19:54:44 +0700

    JS17_Konfigurasi Google Provider di NextAuth dan Handle Callback JWT & Session

--- praktikum/my-app/src/pages/api/auth/[...nextauth].ts
+++ praktikum/my-app/src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { signIn } from "../../../utlis/db/servicefirebase";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -41,6 +42,10 @@ export const authOptions: NextAuthOptions = {
         return null;
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    })
   ],
   callbacks: {
     async jwt({ token, account, profile, user }: any) {
@@ -48,6 +53,20 @@ export const authOptions: NextAuthOptions = {
         token.email = user.email;
         token.fullname = user.fullname;
         token.role = user.role;
+      }
+      // Jika login dengan google, tambahkan informasi yang diperlukan kedalam token
+      if (account?.provider === "google") {
+        const data = {
+          fullname: user.name,
+          email: user.email,
+          image: user.image,
+          type: account.provider,
+        };
+        // console.log("Google login data", { data });
+        token.fullname = data.fullname;
+        token.email = data.email;
+        token.image = data.image;
+        token.type = data.type;
       }
       return token;
     },
@@ -58,8 +77,14 @@ export const authOptions: NextAuthOptions = {
       if (token.fullname) {
         session.user.fullname = token.fullname;
       }
+      if (token.image) {
+        session.user.image = token.image;
+      }
       if (token.role) {
         session.user.role = token.role
+      }
+      if (token.type) {
+        session.user.type = token.type;
       }
       return session;
     },
