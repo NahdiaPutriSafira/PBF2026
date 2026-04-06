76257998cb4ed743d038d59e886780ca403230e8
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-06 21:52:45 +0700

    JS17_Tugas2 dan Tugas 5

---
+++ praktikum/my-app/src/pages/editor/index.tsx
@@ -0,0 +1,18 @@
+const HalamanEditor = () => {
+  return (
+    <div>
+      <div className="editor">
+        <h1>Halaman Editor</h1>
+        <p>
+          Selamat datang di halaman editor! Anda memiliki akses untuk mengelola
+          konten dan melakukan perubahan data sesuai dengan peran editor. Di
+          halaman ini, Anda dapat mengedit informasi, memperbarui data, dan
+          memastikan konten tetap terjaga kualitasnya. Gunakan hak akses ini
+          dengan bijak.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanEditor;
