ce098ed6e07fbdf25f8d78965b78d9d057dfde2d
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-07 21:55:00 +0700

    JS18_Praktikum1B

--- praktikum/my-app/next.config.ts
+++ praktikum/my-app/next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
--- praktikum/my-app/src/views/produk/index.tsx
+++ praktikum/my-app/src/views/produk/index.tsx
@@ -1,5 +1,6 @@
 import styles from "../../pages/produk/produk.module.scss"; 
 import Link from "next/link";
+import Image from "next/image";
 
 type ProductType = {
   id: string;
@@ -16,6 +17,7 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
 
       <div className={styles.produk__content}>
         {products.length > 0 ? (
+          
           products.map((products: ProductType) => (
             <Link
               href={`/produk/${products.id}`}
@@ -23,7 +25,7 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
               className={styles.produk__content__item}
             >
               <div className={styles.produk__content__item__image}>
-                <img src={products.image} alt={products.name} width={200} />
+                <Image src={products.image} alt={products.name} width={200} />
               </div>
 
               <h4 className={styles.produk__content__item__name}>
