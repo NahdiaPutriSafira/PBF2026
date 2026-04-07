8322d77c10e2aea27dcc3a54344baec8aadbb6cb
Nahdia Putri <nptrsfr1808@gmail.com>
2026-04-08 00:22:51 +0700

    JS18_Tugas Praktikum

--- praktikum/my-app/src/pages/_app.tsx
+++ praktikum/my-app/src/pages/_app.tsx
@@ -2,14 +2,33 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppShell from '@/component/layouts/Appshell';
 import Navbar from '@/component/layouts/navbar'
-import {SessionProvider} from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
+import Script from 'next/script'; // ✅ 1. Import next/script
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-    <AppShell>
-      <Component{...pageProps} />
-    </AppShell>
-    </SessionProvider>
+    <>
+      {/* ✅ 2. Tambahkan Script Google Analytics menggunakan next/script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-XYZ1234567"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XYZ1234567');
+        `}
+      </Script>
+
+      {/* Kode bawaan milikmu tetap aman di bawah sini */}
+      <SessionProvider session={pageProps.session}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </SessionProvider>
+    </>
   );
 };
--- praktikum/my-app/src/views/DetailProduct/index.tsx
+++ praktikum/my-app/src/views/DetailProduct/index.tsx
@@ -1,6 +1,7 @@
 // import {ProductType} from "@/types/Product.type";
 import { ProductType } from "../../types/Product.type";
-import styles from "@/views/DetailProduct/detailProduct.module.scss"
+import styles from "@/views/DetailProduct/detailProduct.module.scss";
+import Image from "next/image"; // ✅ 1. Import komponen Image
 
 const DetailProduk = ({ products }: { products: ProductType }) => {
   return (
@@ -8,7 +9,20 @@ const DetailProduk = ({ products }: { products: ProductType }) => {
       <h1 className={styles.title}>Detail Produk</h1>
       <div className={styles.produkdetail}>
         <div className={styles.produkdetail__image}>
-          <img src={products.image && products.image} alt={products.name} />
+          
+          {/* Tag img lama kita matikan/komentar */}
+          {/* <img src={products.image && products.image} alt={products.name} /> */}
+
+          {/* ✅ 2. Gunakan komponen Image dengan width & height */}
+          {products.image && (
+            <Image 
+              src={products.image} 
+              alt={products.name} 
+              width={400}  // Ukuran width bisa kamu sesuaikan
+              height={400} // Ukuran height bisa kamu sesuaikan
+            />
+          )}
+
         </div>
 
         <div className={styles.produkdetail__info}>
